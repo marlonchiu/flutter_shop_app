3d277c27636cae1bb2aa3f125a1ad36569348e67
marlonchiu <Knight1898@outlook.com>
2019-09-30 11:07:38 +0800

    商城首页 轮播图数据展示

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import '../service/service_method.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'dart:convert';
 
 class HomePage extends StatefulWidget {
   @override
@@ -9,16 +11,17 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   String homePageContent = '正在获取数据';
 
-  @override
-  void initState() {
-    getHomePageContent().then((val) {
-      setState(() {
-        homePageContent = val.toString();
-      });
-    });
-    // TODO: implement initState
-    super.initState();
-  }
+  // 直接 FutureBuilder 获取数据 就不初始化了
+  // @override
+  // void initState() {
+  //   getHomePageContent().then((val) {
+  //     setState(() {
+  //       homePageContent = val.toString();
+  //     });
+  //   });
+  //   // TODO: implement initState
+  //   super.initState();
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -26,14 +29,52 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text('百姓生活+'),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Text(homePageContent)
-          ],
-        ),
+      // FutureBuilder Widget 用来等待异步请求的
+      body: FutureBuilder(
+        future: getHomePageContent(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            var data = json.decode(snapshot.data.toString());
+            List<Map> swiperDataList =
+                (data['data']['slides'] as List).cast(); // 顶部轮播组件数
+            return Column(
+              children: <Widget>[
+                SwiperDiy( // 页面顶部轮播组件
+                  swiperDataList: swiperDataList,
+                )
+              ],
+            );
+          } else {
+            return Center(
+              child: Text('加载中。。。'),
+            );
+          }
+        },
       ),
     );
   }
+}
 
+// 首页轮播组件
+class SwiperDiy extends StatelessWidget {
+  final List swiperDataList;
+  SwiperDiy({Key key, this.swiperDataList}) : super(key: key);
+  // 上句话简写
+  // SwiperDiy({this.swiperDataList});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 333.0,
+      child: Swiper(
+        itemBuilder: (BuildContext content, int index) {
+          return Image.network("${swiperDataList[index]['image']}",
+              fit: BoxFit.fill);
+        },
+        itemCount: swiperDataList.length,
+        pagination: new SwiperPagination(),
+        autoplay: true,
+      ),
+    );
+  }
 }
