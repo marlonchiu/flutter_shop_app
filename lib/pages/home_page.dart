f107f0de12ef4d7da364c2c5c56e4ba528ed6cd0
marlonchiu <Knight1898@outlook.com>
2019-09-30 12:02:44 +0800

    商城首页 导航部件展示

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -38,11 +38,16 @@ class _HomePageState extends State<HomePage> {
             var data = json.decode(snapshot.data.toString());
             List<Map> swiperDataList =
                 (data['data']['slides'] as List).cast(); // 顶部轮播组件数
+            List<Map> navigatorList =
+                (data['data']['category'] as List).cast(); //类别列表
             return Column(
               children: <Widget>[
-                SwiperDiy( // 页面顶部轮播组件
+                SwiperDiy(
+                  // 页面顶部轮播组件
                   swiperDataList: swiperDataList,
-                )
+                ),
+                TopNavigator( // 导航栏目
+                  navigatorList: navigatorList,)
               ],
             );
           } else {
@@ -66,7 +71,8 @@ class SwiperDiy extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // iphone6 作为参考设计尺寸
-    ScreenUtil.instance = ScreenUtil(width: 750, height: 1334)..init(context);
+    // 放在全局引入
+    // ScreenUtil.instance = ScreenUtil(width: 750, height: 1334)..init(context);
     // print('设备宽度:${ScreenUtil.screenWidth}');
     // print('设备高度:${ScreenUtil.screenHeight}');
     // print('设备像素密度:${ScreenUtil.pixelRatio}');
@@ -81,6 +87,47 @@ class SwiperDiy extends StatelessWidget {
         itemCount: swiperDataList.length,
         pagination: new SwiperPagination(),
         autoplay: true,
+      ),
+    );
+  }
+}
+
+// 导航区域
+class TopNavigator extends StatelessWidget {
+  final List navigatorList;
+  TopNavigator({Key key, this.navigatorList}) : super(key: key);
+
+  // 把每一个导航看做一个单独的组件
+  Widget _gridViewItemUI(BuildContext context, item) {
+    return InkWell(
+      onTap: () {
+        print('点击导航');
+      },
+      child: Column(
+        children: <Widget>[
+          Image.network(item['image'], width: ScreenUtil().setWidth(95)),
+          Text(item['mallCategoryName'])
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // 边界判断
+    if(navigatorList.length > 10) {
+      navigatorList.removeRange(10, navigatorList.length);
+    }
+
+    return Container(
+      height: ScreenUtil().setHeight(320.0),
+      padding: EdgeInsets.all(3.0),
+      child: GridView.count(
+        crossAxisCount: 5,
+        padding: EdgeInsets.all(4.0),
+        children: navigatorList.map((item){
+          return _gridViewItemUI(context, item);
+        }).toList(),
       ),
     );
   }
