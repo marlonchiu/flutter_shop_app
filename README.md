0c4bec5bfdd15701a4132d423ff7794c89daccab
marlonchiu <Knight1898@outlook.com>
2020-01-04 13:31:09 +0800

    分类接口异常，模拟json 数据

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-## 写在前面
+# 写在前面
+
+## 资源地址
 
 * [掘金小册 -- 视频 Flutter 实战详解：企业级移动电商开发](https://juejin.im/book/5cdbc3615188254b1a0743f1)
 
@@ -6,3 +8,7 @@
 
 * [B站视频地址--  技术胖flutter移动电商实战](https://www.bilibili.com/video/av62419128?t=46&p=1)
 
+## 用到的网址
+
+* [在线JSON校验格式化工具（Be JSON）](http://www.bejson.com/)
+* [JSON to Dart](https://javiercbk.github.io/json_to_dart/)
--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -1,7 +1,16 @@
+// 使用该库中的 rootBundle 对象来读取 perosn.json 文件
+import 'package:flutter/services.dart';
+
 import 'package:flutter/material.dart';
 import '../service/service_method.dart';
 import 'dart:convert';
 import '../modle/category.dart';
+// 因为接口数据异常 所以此处使用 json 数据模拟一下
+
+// 读取 assets 文件夹中的 category.json 文件
+Future<String> _loadCategoryJson() async {
+  return await rootBundle.loadString('assets/category.json');
+}
 
 class CategoryPage extends StatefulWidget {
   @override
@@ -11,22 +20,45 @@ class CategoryPage extends StatefulWidget {
 class _CategoryPageState extends State<CategoryPage> {
   @override
   Widget build(BuildContext context) {
-    _getCategory();
+    // _getCategory();
     return Container(
       child: Center(
         child: Text('商城分类'),
       ),
     );
   }
+}
+
+// 左侧导航菜单
+class LeftCategoryNav extends StatefulWidget {
+  @override
+  _LeftCategoryNavState createState() => _LeftCategoryNavState();
+}
+
+class _LeftCategoryNavState extends State<LeftCategoryNav> {
+  List list = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 
   // 获取商城分类数据
+
   void _getCategory() async {
-    await commonRequest('getCategory').then((val) {
-      var data = json.decode(val.toString());
-      print(data['data']);
-      CategoryBigListModel list = CategoryBigListModel.fromJson(data['data']);
-      list.data.forEach((item) => print(item));
-      // print(category);
-    });
+    // 模拟json 数据
+    var val = await _loadCategoryJson();
+    var data = json.decode(val.toString());
+    print(data['data']);
+
+    // 接口调用
+    // await commonRequest('getCategory').then((val) {
+
+    //   var data = json.decode(val.toString());
+    //   print(data);
+    //   CategoryModel category = CategoryModel.fromJson(data);
+    //   print(category);
+    //   category.data.forEach((item) => print(item));
+    // });
   }
 }
