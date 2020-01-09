b622986b92625a9792494af9b8e0a31148a4cb66
marlonchiu <Knight1898@outlook.com>
2020-01-09 21:56:57 +0800

    65.会员中心_订单区域UI布局

--- lib/pages/member_page.dart
+++ lib/pages/member_page.dart
@@ -9,7 +9,10 @@ class MemberPage extends StatelessWidget {
         title: Text('会员中心'),
       ),
       body: ListView(
-        children: <Widget>[_topHeader()],
+        children: <Widget>[
+          _topHeader(),
+          _orderTitle(),
+          _orderTypeList()],
       ),
     );
   }
@@ -37,6 +40,55 @@ class MemberPage extends StatelessWidget {
               ),
             ),
           )
+        ],
+      ),
+    );
+  }
+
+  // 我的订单顶部
+  Widget _orderTitle() {
+    return Container(
+      margin: EdgeInsets.only(top: 10),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(bottom: BorderSide(width: 1, color: Colors.black12))),
+      child: ListTile(
+        leading: Icon(Icons.list),
+        title: Text('我的订单'),
+        trailing: Icon(Icons.arrow_right),
+      ),
+    );
+  }
+
+  // 订单列表区域
+  Widget _orderTypeList() {
+    return Container(
+      margin: EdgeInsets.only(top: 5),
+      width: ScreenUtil().setWidth(750),
+      height: ScreenUtil().setHeight(150),
+      padding: EdgeInsets.only(top: 20),
+      color: Colors.white,
+      child: Row(
+        children: <Widget>[
+          _orderTypeItem('待付款', Icons.party_mode),
+          _orderTypeItem('待发货', Icons.query_builder),
+          _orderTypeItem('待收货', Icons.directions_car),
+          _orderTypeItem('待评价', Icons.content_paste),
+        ],
+      ),
+    );
+  }
+
+  Widget _orderTypeItem(String title, icon) {
+    return Container(
+      width: ScreenUtil().setWidth(187),
+      child: Column(
+        children: <Widget>[
+          Icon(
+            icon,
+            size: 30,
+          ),
+          Text(title),
         ],
       ),
     );
